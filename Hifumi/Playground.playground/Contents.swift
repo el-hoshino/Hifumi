fc1f7777a0e4b343a0ba20c13fa7de8390b52a06
星野恵瑠 <shonetesmith@gmail.com>
2017-11-29 02:16:29 +0900

    Use PartialRange in Playground

--- Hifumi/Playground.playground/Contents.swift
+++ Hifumi/Playground.playground/Contents.swift
@@ -7,5 +7,5 @@ import Hifumi
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 let url = Bundle.main.url(forResource: "5370", withExtension: "mp3")!
-let player = try! HifumiPlayer(url: url, playMode: .loop(range: 2205 ... 352800))
+let player = try! HifumiPlayer(url: url, playMode: .loop(range: 2205...))
 player.play()
