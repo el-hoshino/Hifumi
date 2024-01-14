a4ed381554ca6acf8ad42cfad62d9d06ef4e7745
lovee <shonetesmith@gmail.com>
2024-01-14 23:42:56 +0900

    Add headers output
    so Objective-C projects can use it

--- Package.swift
+++ Package.swift
@@ -24,7 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Hifumi",
-            dependencies: []),
+            dependencies: [],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+            ]),
         .testTarget(
             name: "HifumiTests",
             dependencies: ["Hifumi"]),
